453bcb3bdc8a4ba26abb128e0cfe518c9f31f9ea
Lorand Szakacs <lsz@lorandszakacs.com>
2018-04-24 15:54:07 +0300

    Remove needless nesting + remove spray

--- lab_05/src/main/scala/com/mihai/api/UserApi.scala
+++ lab_05/src/main/scala/com/mihai/api/UserApi.scala
@@ -1,24 +1,29 @@
 package com.mihai.api
 
-import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
+import busymachines.json._
+import busymachines.rest.jsonrest._
+
 import akka.http.scaladsl.server.Directives.{complete, get, path, post}
 import com.mihai.datamodel.User
 import com.mihai.mockdatabase.Database
-import spray.json.{DefaultJsonProtocol, RootJsonFormat}
-import akka.http.scaladsl.server.Directives._
-trait UserApi extends SprayJsonSupport with DefaultJsonProtocol {
 
-  implicit val userSerializer: RootJsonFormat[User] = jsonFormat4(User)
+import akka.http.scaladsl.server.Directives._
+
+trait UserApi extends JsonSupport {
+
+  implicit val userSerializer: Codec[User] = derive.codec[User]
 
   val userRoute =
     path("user") {
       get {
         complete(Database.getAllUsers())
       } ~
-      post { entity(as[User]) { usr =>
-        complete(Database.addUser(usr))
-      }}
-    } ~ path("user" / Segment) {userId =>
+        post {
+          entity(as[User]) { usr =>
+            complete(Database.addUser(usr))
+          }
+        }
+    } ~ path("user" / Segment) { userId =>
       delete(
         complete(Database.deleteUser(userId))
       )
