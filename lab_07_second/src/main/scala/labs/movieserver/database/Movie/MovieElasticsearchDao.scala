c777f0da10096ae4a413b67cf72a190dcca09bc4
Dan Vidican <vidican.dan@gmail.com>
2018-05-15 15:20:51 +0300

    movie CRUD works

--- lab_07_second/src/main/scala/labs/movieserver/database/Movie/MovieElasticsearchDao.scala
+++ lab_07_second/src/main/scala/labs/movieserver/database/Movie/MovieElasticsearchDao.scala
@@ -3,21 +3,28 @@ package labs.movieserver.database.Movie
 import com.sksamuel.elastic4s.ElasticsearchClientUri
 import com.sksamuel.elastic4s.analyzers.StopAnalyzer
 import com.sksamuel.elastic4s.http.ElasticDsl._
-import com.sksamuel.elastic4s.http.HttpClient
+import com.sksamuel.elastic4s.http.get.GetResponse
+import com.sksamuel.elastic4s.http.{ElasticDsl, HttpClient, RequestFailure, RequestSuccess}
 
 import scala.util.Random
 //import labs.movieserver.datamodel.{MovieWithoutId}
 import labs.movieserver.datamodel._
 import spray.json.DefaultJsonProtocol._
+import spray.json.{RootJsonFormat}
+import spray.json._
+
 
 import scala.concurrent.duration._
 import scala.concurrent.{ExecutionContext, Future, _}
 
+
 class MovieElasticsearchDao(implicit val ex: ExecutionContext) extends MovieDao {
 
-  implicit val movieFormat  = jsonFormat3(MovieWithoutId)
+  implicit val movieFormat: RootJsonFormat[MovieWithoutId] = jsonFormat3(MovieWithoutId)
+  implicit val movieIdFormat  = jsonFormat4(Movie)
 
-  val elasticClient = HttpClient(ElasticsearchClientUri("localhost", 9202))
+
+  val elasticClient = HttpClient(ElasticsearchClientUri("localhost", 9200))
   val indexName = "lab_07"
   val typeName: String = "movie"
 
@@ -27,18 +34,47 @@ class MovieElasticsearchDao(implicit val ex: ExecutionContext) extends MovieDao 
   override def addMovie(movie: MovieWithoutId): Future[String] =
     for {
       movieId <- getFreeId
+      movieWithId = Movie(movieId, movie.title, movie.year, movie.rating)
       _ <- elasticClient.execute {
-        (indexInto (indexName, typeName) doc movieFormat.write(movie).toJson.toString() id movieId)
+        (indexInto (indexName, typeName) doc movieWithId.toJson.toString() id movieId)
       }
     } yield movieId
 
-  override def deleteMovie(id: String): Future[String] = ???
+  override def deleteMovie(movieId: String): Future[String] = for {
+    _ <- elasticClient.execute {
+      ElasticDsl.delete(movieId) from (indexName,typeName)
+    }
+  } yield (movieId)
 
-  override def getAllMovies: Future[Seq[Movie]] = ???
+  override def getAllMovies: Future[Seq[Movie]] =
+    for {
+      results <- elasticClient.execute {
+          search(indexName)
+      }
+      movies = results match {
+          case Right(r) => r.result.hits.hits.map(x => {x.sourceAsString.parseJson.convertTo[Movie]}).toList
+          case _ => throw new Exception("Movie database is empty!")
+      }
+  } yield movies
 
-  override def getMovie(movieId: String): Future[Movie] = ???
+  override def getMovie(movieId: String): Future[Movie] =
+    for {
+      result <- elasticClient.execute {
+        get(indexName,typeName,movieId)
+      }
+      response = result.getOrElse(throw RequestFailed())
+      _ = if(response.result.isSourceEmpty || !response.result.exists) throw MovieNotFoundException(movieId)
+      movie = response.result.sourceAsString.parseJson.convertTo[Movie]
+    } yield movie
 
-  override def updateMovie(id: String, movie: MovieWithoutId): Future[String] = ???
+  override def updateMovie(movieId: String, movie: MovieWithoutId): Future[String] =
+    for {
+      m <- getMovie(movieId)
+      newMovie = Movie(m.id, movie.title, movie.year, movie.rating)
+      _ <- elasticClient.execute {
+        (indexInto (indexName, typeName) doc newMovie.toJson.toString() id movieId)
+      }
+    } yield movieId
 
 
 
@@ -47,9 +83,10 @@ class MovieElasticsearchDao(implicit val ex: ExecutionContext) extends MovieDao 
       indexExists <- elasticClient.execute { indexExists(indexName) }
       _ <- if (indexExists.isRight) Future.successful(())
       else elasticClient.execute {
-              createIndex(indexName).mappings(
+              createIndex(indexName)
+                .mappings(
                 mapping(typeName) as (
-                  keywordField("id"),
+                  nestedField("id"),
                   textField("title"),
                   intField("year"),
                   doubleField("rating") analyzer StopAnalyzer
@@ -59,8 +96,11 @@ class MovieElasticsearchDao(implicit val ex: ExecutionContext) extends MovieDao 
     } yield ()
 
 
+
+
+
   private def getFreeId: Future[String] = Future.successful(Random.nextInt(100000).toString)
-  //{
+//  {
 //    val newId: String = Random.nextInt(100000).toString
 //    for {
 //      allMovies <- getAllMovies
